9e5b3cdc8991282245da38842fc462bfdcfe6454
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-11-05 15:31:59 +0100

    Avoid having double relations

--- src/builders/mermaid/Mermaid.ts
+++ src/builders/mermaid/Mermaid.ts
@@ -12,6 +12,8 @@ export class MermaidBuilder implements IBuilder {
 
   private diagram = "";
 
+  private processedRelations = new Set<string>();
+
   constructor() {
     this.diagram = "erDiagram\n";
   }
@@ -32,8 +34,15 @@ export class MermaidBuilder implements IBuilder {
     entity.relations.forEach((relation) => {
       const relatedEntityName = relation.type.replace(/\[\]/, '');
       const relationType = this.mappedRelationTypes[relation.relation];
-      
-      this.diagram += `  ${entity.name} ${relationType} ${relatedEntityName}: "${relation.relation}"\n`;
+
+      const relationKey = `${entity.name}-${relatedEntityName}`;
+      const reverseRelationKey = `${relatedEntityName}-${entity.name}`;
+
+      // Check if the relation or its reverse has already been processed (For mermaid, one is enough)
+      if (!this.processedRelations.has(relationKey) && !this.processedRelations.has(reverseRelationKey)) {
+        this.diagram += `  ${entity.name} ${relationType} ${relatedEntityName}: "${relation.relation}"\n`;
+        this.processedRelations.add(relationKey);
+      }
     });
   }
 }
