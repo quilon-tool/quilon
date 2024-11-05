1cc0716c8fcc381085dac5f827c18f5eaa48fd98
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-11-05 15:57:13 +0100

    Update InitCommand

--- src/commands/init/InitCommand.ts
+++ src/commands/init/InitCommand.ts
@@ -1,24 +1,24 @@
 import fs from "fs";
-import { ConfigFile, DiagramLanguages, ORMs } from "../../global/types";
-import config from "../../global/Config";
+import { IConfigFile, DiagramLanguages, ORMs } from "../../global/types";
+import { GlobalConfig } from "../../global/Config";
 import { AbstractCommand } from "../AbstractCommand";
 
 // TODO: Create JSON Schema that gets automatically injected as "$schema" in quilon.json (Useful for autocompletion and validation)
 
 export class InitCommand extends AbstractCommand {
-  private DEFAULT_CONFIG: ConfigFile = {
+  private DEFAULT_CONFIG: IConfigFile = {
     entities: [],
     orm: ORMs.TypeORM,
     diagramLanguage: DiagramLanguages.Mermaid
   }
 
  execute(): void {
-  if (fs.existsSync(config.configPath)) {
-    console.error(`${config.configFile} already exists.`);
+  if (fs.existsSync(GlobalConfig.CONFIG_PATH)) {
+    console.error(`${GlobalConfig.CONFIG_FILE} already exists.`);
     return;
   }
 
-  fs.writeFileSync(config.configPath, JSON.stringify(this.DEFAULT_CONFIG, null, 2));
-  console.log(`${config.configFile} successfully created.`);
+  fs.writeFileSync(GlobalConfig.CONFIG_PATH, JSON.stringify(this.DEFAULT_CONFIG, null, 2));
+  console.log(`${GlobalConfig.CONFIG_FILE} successfully created.`);
  } 
 }
