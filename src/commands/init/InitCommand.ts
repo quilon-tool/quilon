26cf84b56f81058a45fa7e458a6bed3e0f712f6b
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-11-15 13:12:27 +0100

    Add JSON Schema to init command

--- src/commands/init/InitCommand.ts
+++ src/commands/init/InitCommand.ts
@@ -5,6 +5,7 @@ import { AbstractCommand } from "../AbstractCommand";
 
 export class InitCommand extends AbstractCommand {
   private DEFAULT_CONFIG: IConfigFile = {
+    $schema: "https://raw.githubusercontent.com/quilon-tool/quilon/refs/heads/main/src/config/config-schema.json",
     entities: [],
     orm: ORMs.TypeORM,
     diagramLanguage: DiagramLanguages.Mermaid,
