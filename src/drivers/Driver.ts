a86720251a1938a280d086003a11a74d365a1dd9
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-11-15 20:21:02 +0100

    Add JSDocs to Driver

--- src/drivers/Driver.ts
+++ src/drivers/Driver.ts
@@ -15,10 +15,21 @@ export class Driver implements IDriver {
     this.orm = orm;
   }
 
+  /**
+   * Sets the file path for the entity to be parsed.
+   * 
+   * @param {string} filePath - The path of the file to parse.
+   */
   setFilePath(filePath: string): void {
     this.filePath = filePath;
   }
 
+   /**
+   * Parses an entity from the specified file path using the appropriate ORM driver.
+   * 
+   * @returns {IEntityData} The parsed entity data.
+   * @throws {Error} If the file path is not set or if no suitable driver is found.
+   */
   parseEntity(): IEntityData {
     if (!this.filePath) {
       throw new Error("filePath is not defined.");
@@ -28,6 +39,12 @@ export class Driver implements IDriver {
     return driver.parseEntity(this.filePath);
   }
 
+  /**
+   * Returns the filename pattern to match entity files for the configured ORM.
+   * 
+   * @returns {string} The file name pattern.
+   * @throws {Error} If no pattern is implemented for the configured ORM.
+   */
   getFileNamePattern(): string {
     switch(this.orm) {
       case ORMs.TypeORM:
@@ -37,6 +54,13 @@ export class Driver implements IDriver {
     }
   }
 
+  /**
+   * Returns the appropriate driver instance based on the configured ORM.
+   * 
+   * @private
+   * @returns {IDriver} The driver instance for the specified ORM.
+   * @throws {Error} If the file path is not set or if no suitable driver is found.
+   */
   private getDriver(): IDriver {
     if (!this.filePath) {
       throw new Error("filePath is not defined.");
